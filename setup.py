ec4a0a0865adb84a183b31d465c553e124904445
ademic2022 <adeeyomichael77@gmail.com>
2025-06-12 19:53:35 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+import os
+import re
+
+from setuptools import find_packages, setup
+
+
+def get_long_description():
+    for filename in ("README.rst",):
+        with open(filename, "r") as f:
+            yield f.read()
+
+
+def get_version(package):
+    with open(os.path.join(package, "__init__.py")) as f:
+        pattern = r'^__version__ = [\'"]([^\'"]*)[\'"]'
+        return re.search(pattern, f.read(), re.MULTILINE).group(1)
+
+
+setup(
+    name="django-graphql-social-auth",
+    version=get_version("graphql_social_auth"),
+    license="MIT",
+    description="Python Social Auth support for Django GraphQL",
+    long_description="\n\n".join(get_long_description()),
+    author="mongkok",
+    author_email="domake.io@gmail.com",
+    maintainer="Michael",
+    maintainer_email="ademictech@gmail.com",
+    url="https://github.com/flavors/django-graphql-social-auth/",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[
+        "Django>=3.2",
+        "graphene-django>=3.0.0",
+        "social-auth-app-django>=5.0.0",
+        "django-filter>=25.0",
+    ],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Framework :: Django",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "Framework :: Django :: 4.1",
+        "Framework :: Django :: 4.2",
+        "Framework :: Django :: 5.0",
+        "Framework :: Django :: 5.1",
+    ],
+    zip_safe=False,
+    tests_require=[
+        "Django>=3.2",
+        "graphene-django>=3.0.0",
+        "social-auth-app-django>=5.0.0",
+    ],
+    package_data={
+        "graphql_social_auth": [
+            "locale/*/LC_MESSAGES/django.po",
+            "locale/*/LC_MESSAGES/django.mo",
+        ],
+    },
+    extras_require={
+        "jwt": ["django-graphql-jwt>=0.4.0"],
+    },
+)
---
+++ tests/mixins.py
@@ -0,0 +1,36 @@
+import sys
+from unittest.mock import patch
+
+from .decorators import social_auth_mock
+
+
+class SocialAuthMixin:
+
+    @social_auth_mock
+    @patch("graphql_social_auth.decorators._do_login")
+    def test_social_auth(self, *args):
+        response = self.execute(
+            {
+                "provider": "google-oauth2",
+                "accessToken": "-token-",
+            }
+        )
+
+        social = response.data["socialAuth"]["social"]
+        self.assertEqual("test", social["uid"])
+
+
+class SocialAuthJWTMixin:
+
+    @social_auth_mock
+    @patch.dict(sys.modules, {"graphql_jwt.shortcuts": None})
+    def test_social_auth_import_error(self, *args):
+        response = self.execute(
+            {
+                "provider": "google-oauth2",
+                "accessToken": "-token-",
+            }
+        )
+
+        self.assertTrue(response.errors)
+        self.assertIsNone(response.data["socialAuth"])
